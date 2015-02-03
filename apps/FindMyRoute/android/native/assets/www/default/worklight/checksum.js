4111d88918aba57f0d4d61a555fafcf260b34197
Andi Setiyadi <setiyadi@us.ibm.com>
2015-02-03 13:45:37 -0700

    Initial commit

---
+++ apps/FindMyRoute/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":3678752665,"date":1422994288465,"machine":"ks-RiverRun"};
+/* Date: Tue Feb 03 13:11:28 MST 2015 */
---
+++ apps/FindMyRoute/android/native/src/com/FindMyRoute/ForegroundService.java
@@ -0,0 +1,5 @@
+package com.FindMyRoute;
+
+public class ForegroundService extends com.worklight.androidgap.WLForegroundService{
+	//Nothing to do here...
+}
