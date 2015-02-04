9532aa355f76846f0aabf4511e5746a83147a861
Andi Setiyadi <setiyadi@us.ibm.com>
2015-02-04 13:20:36 -0700

    Update SDK and G Play Services library location

--- apps/FindMyRoute/android/native/assets/www/default/worklight/checksum.js
+++ apps/FindMyRoute/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":3678752665,"date":1422994288465,"machine":"ks-RiverRun"};
-/* Date: Tue Feb 03 13:11:28 MST 2015 */
+var WL_CHECKSUM = {"checksum":3678752665,"date":1423081085817,"machine":"ks-RiverRun"};
+/* Date: Wed Feb 04 13:18:05 MST 2015 */
